9967c82104e1371110398a6059901ae1fb5e7447
Ali Hussain <alihussainn124@gmail.com>
2025-08-01 20:57:56 +0500

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -30,10 +30,68 @@ void recursiveDisplay(Node *head){
 	cout<<head->data<<"->";
 	return recursiveDisplay(head->next);
 }
+//counting nodes in a linked list
+int countNodes(Node* head){
+	Node* temp = head;
+	int count=0;
+	while(temp != 0){
+		count++;
+		temp = temp->next;
+	}
+	return count;
+}
+//recursive counting
+int recursiveCount(Node* head){
+	if(head==0){
+		return 0;
+	}
+	return 1+recursiveCount(head->next);
+}
+//sum of all elements in a linked list
+int addElements(Node * head){
+	int sum = 0;
+	Node* temp = head;
+	while(temp != 0){
+		sum = sum + temp->data;
+		temp= temp->next;
+	}
+	return sum;
+}
+//recursive sum of all the elements 
+int recursiveSum(Node* head){
+	if(head==0){
+		return 0;
+	}
+	return head->data + recursiveSum(head->next);
+}
+//finding maximum element in a linkedList
+void printmaxElement(Node *head){
+	Node* temp = head;
+	int max;
+	max = temp->data;
+	while(temp!= 0){
+		if(temp->data > max){
+			max = temp->data;
+		}
+		temp = temp->next;
+	}
+	cout<<"Maximum element = "<<max<<endl;
+}
+//recursive function to find max
+int recursiveMaxelement(Node* head){
+	if(head == 0){
+		return 
+	}
+}
 int main(){
 	Node* head = new Node(10);
-	head->next = new Node(20);
+	head->next = new Node(40);
 	head->next->next = new Node(30);
 	displayLinkedlist(head);
 	recursiveDisplay(head);
+	int totalNodes = recursiveCount(head);
+	cout<<totalNodes<<endl;
+	int sum = recursiveSum(head);
+	cout<<sum<<endl;
+	printmaxElement(head);
 }
